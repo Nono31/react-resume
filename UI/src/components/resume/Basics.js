f227fe0820602c9bc70b21658737a736cb1c9350
Arnaud Druffin <arnaud.druffin@orange.com>
2018-01-05 16:24:12 +0100

    Update styles

--- UI/src/components/resume/Basics.js
+++ UI/src/components/resume/Basics.js
@@ -4,13 +4,17 @@ import PropTypes from 'prop-types';
 const Basics = ({ resume }) => {
   return (
     <div className="row">
-      <div className="col-xs-12">
+      <div className="col-sm-12">
         <div id="photo-header" className="text-center">
           <div id="photo">
-            <img src={resume.basics.photo} alt="avatar" />
+            <img src="http://www.gravatar.com/avatar/9acdb04e603c1cac2b893190f6dd5911?s=200&r=pg&d=mm" alt="avatar" />
           </div>
           <div id="text-header" >
-            <h1>{resume.basics.name}<br />{resume.basics.label}<span>{resume.basics.label}</span>{resume.basics.label}</h1>
+            <h1>
+            {resume.basics.name}
+            <br />
+            <span>{resume.basics.label}</span>
+            </h1>
           </div>
         </div>
       </div>
